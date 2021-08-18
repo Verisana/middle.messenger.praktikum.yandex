cf63207412307b339af48ea9f5bbd08697942194
Verisana <account@0112.pro>
2021-08-18 10:56:58 +0500

    feat: make root in html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { sum } from './modules/sum'
+import { sum } from "./modules/sum"
 
-const root = document.querySelector('#root')
+const root = document.querySelector("#root")
 root.textContent = sum(6, -1).toString()
