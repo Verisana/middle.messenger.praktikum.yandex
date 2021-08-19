9c0a30de3725d187dfbb475a04c05657c7b5a0be
Verisana <account@0112.pro>
2021-08-19 17:58:04 +0500

    feat: make tests checks to work with new index.html

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import { sum } from "./modules/sum"
 import testHBS from "./test.hbs"
 import main from "./main"
 
-const root = document.querySelector("#root")
-const sumHTML = document.querySelector("#sum")
-sumHTML.textContent = `Sum checking ${sum(6, -1).toString()}`
+const root = document.createElement("div")
+document.body.appendChild(root)
 
-const hbs = root.querySelector("#testHBS")
+const p_sum = document.createElement("p")
+p_sum.textContent = `Sum checking ${sum(6, -1).toString()}`
+
+root.appendChild(p_sum)
+
+const hbs = document.createElement("div")
 
 const params = { firstName: "Handlebars", lastName: "Test" }
 hbs.innerHTML = testHBS(params)
 
+document.body.appendChild(hbs)
 document.body.appendChild(main())
