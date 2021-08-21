308ece8a610afd4619f9c0a590c35379e060e651
Verisana <account@0112.pro>
2021-08-21 22:54:35 +0500

    feat: move test buttons to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,11 @@
 import footerTemplate from "./footer.hbs"
+import { string2DomElement } from "../../utils/utils.js"
+import { linkButtons } from "../../router/tempButtons.js"
 
-const footer = new DOMParser().parseFromString(footerTemplate(), "text/html")
-    .body.firstChild
+const footer = string2DomElement(footerTemplate())
+
+for (const [, button] of Object.entries(linkButtons)) {
+    footer.appendChild(button)
+}
 
 export { footer }
