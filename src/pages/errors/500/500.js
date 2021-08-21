59e10e6eef3440bca23caf59fd3e8fbdd719bb63
Verisana <account@0112.pro>
2021-08-21 15:16:29 +0500

    feat: create server error page placeholder

---
+++ src/pages/errors/500/500.js
@@ -0,0 +1,7 @@
+import classes from "./500.css"
+import serverErrorTemplate from "./500.hbs"
+import { string2DomElement } from "../../../utils/utils.js"
+
+const serverErrorContent = () => string2DomElement(serverErrorTemplate())
+
+export { serverErrorContent }
