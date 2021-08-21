7f969a2132ba6c25930c6c7370542b03bcef8351
Verisana <account@0112.pro>
2021-08-21 15:12:44 +0500

    feat: remove unused test files and update index to render home page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-import { sum } from "./modules/sum"
-import testHBS from "./test.hbs"
-import main from "./main"
+import { switchContent } from "./router/router.js"
 
-const root = document.createElement("div")
-document.body.appendChild(root)
-
-const p_sum = document.createElement("p")
-p_sum.textContent = `Sum checking ${sum(6, -1).toString()}`
-
-root.appendChild(p_sum)
-
-const hbs = document.createElement("div")
-
-const params = { firstName: "Handlebars", lastName: "Test" }
-hbs.innerHTML = testHBS(params)
-
-document.body.appendChild(hbs)
-document.body.appendChild(main())
+switchContent("home")
