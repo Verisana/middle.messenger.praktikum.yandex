aedff8dc3a8532ba9c10901f0f0b4d5e21e22d4b
Verisana <account@0112.pro>
2021-08-21 22:55:54 +0500

    feat: create page structure for 404

--- src/pages/errors/404/404.js
+++ src/pages/errors/404/404.js
@@ -1,7 +1,13 @@
 import classes from "./404.css"
 import errorTemplate from "./404.hbs"
 import { string2DomElement } from "../../../utils/utils.js"
+import { linkButtons } from "../../../router/tempButtons.js"
 
-const errorContent = () => string2DomElement(errorTemplate())
+const errorContent = () => {
+    const content = string2DomElement(errorTemplate())
+    const buttonPlace = content.querySelector("#linkToHome")
+    buttonPlace.appendChild(linkButtons.home)
+    return content
+}
 
 export { errorContent }
