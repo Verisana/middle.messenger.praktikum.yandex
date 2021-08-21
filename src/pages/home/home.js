3b4c46f9e24da5fb0c476d174116e16df54f3a79
Verisana <account@0112.pro>
2021-08-21 22:56:50 +0500

    feat: remove buttons from home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,10 @@
 import classes from "./home.css"
 import homeTemplate from "./home.hbs"
 import { string2DomElement } from "../../utils/utils.js"
-import { switchContent } from "../../router/router.js"
-import { linkButtons } from "../../router/tempButtons"
 
 const homeContent = () => {
     const content = string2DomElement(homeTemplate())
-    for (const [buttonName, button] of Object.entries(linkButtons)) {
-        content.appendChild(button)
-    }
-
     return content
 }
+
 export { homeContent }
