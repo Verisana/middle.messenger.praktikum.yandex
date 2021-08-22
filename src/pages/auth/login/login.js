fb7737a71470dc550f296095c51a993ca130f3e3
Verisana <account@0112.pro>
2021-08-22 19:20:17 +0500

    feat: update login pages

--- src/pages/auth/login/login.js
+++ src/pages/auth/login/login.js
@@ -1,7 +1,21 @@
 import classes from "./login.css"
 import loginTemplate from "./login.hbs"
 import { string2DomElement } from "../../../utils/utils.js"
+import { linkButtons } from "../../../router/tempButtons"
 
-const loginContent = () => string2DomElement(loginTemplate())
+const loginContent = () => {
+    const params = {
+        inputPlace: "login-input",
+        buttonToHome: "login-no-account-button"
+    }
+
+    const content = string2DomElement(loginTemplate(params))
+    const inputPlace = content.querySelector(`#${params.inputPlace}`)
+    const buttonPlace = content.querySelector(`#${params.buttonToHome}`)
+
+    // inputPlace.appendChild()
+    buttonPlace.appendChild(linkButtons.register)
+    return content
+}
 
 export { loginContent }
