e02d5eef79a8cbcaab729a5ac79817a2b2036167
Verisana <account@0112.pro>
2021-08-23 23:40:05 +0500

    feat: create functioning inputfield component

--- src/components/inputField/inputField.js
+++ src/components/inputField/inputField.js
@@ -1,6 +1,26 @@
+import styles from "./inputField.css"
 import inputFieldTemplate from "./inputField.hbs"
-import { string2DomElement } from "../../utils/utils.js"
+import { string2DomElement, convertStyles2Strings } from "../../utils/utils.js"
 
-export const inputField = ({  } = {}) => {
-
+export const inputField = ({ label_, input_, br_ } = {}) => {
+    const params = {}
+    params.label_ =
+        label_ === undefined
+            ? undefined
+            : {
+                  text: label_.text,
+                  class_: convertStyles2Strings(styles, label_.class_)
+              }
+    params.input_ =
+        input_ === undefined
+            ? undefined
+            : {
+                  class_: convertStyles2Strings(styles, input_.class_),
+                  type: input_.type,
+                  required: input_.required,
+                  pattern: input_.pattern
+        }
+    const content = string2DomElement(inputFieldTemplate(params))
+    if (br_) content.appendChild(document.createElement("br"))
+    return content
 }
