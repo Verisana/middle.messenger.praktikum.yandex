152c98216c6faec3a84428ff42628e694a689260
Verisana <account@0112.pro>
2021-08-23 23:41:05 +0500

    feat: check if inputField and submitForm works

--- src/pages/auth/login/login.js
+++ src/pages/auth/login/login.js
@@ -2,6 +2,26 @@ import classes from "./login.css"
 import loginTemplate from "./login.hbs"
 import { string2DomElement } from "../../../utils/utils.js"
 import { linkButtons } from "../../../router/tempButtons"
+import { submitForm } from "../../../components/submitForm/index"
+import { inputField } from "../../../components/inputField/index"
+import { button } from "../../../components/button/index"
+
+const buildLoginForm = () => {
+    const inputBuilders = [
+        inputField.bind(null, {
+            label_: { text: "testName" },
+            input_: { type: "text" },
+            br_: true
+        }),
+        inputField.bind(null, {
+            label_: { text: "testName2" },
+            input_: { type: "text" },
+            br_: true
+        })
+    ]
+    const submitBuilder = button.bind(null, { text: "Submit", type_: "submit" })
+    return submitForm({ inputBuilders, submitBuilder })
+}
 
 const loginContent = () => {
     const params = {
@@ -13,7 +33,7 @@ const loginContent = () => {
     const inputPlace = content.querySelector(`#${params.inputPlace}`)
     const buttonPlace = content.querySelector(`#${params.buttonToHome}`)
 
-    // inputPlace.appendChild()
+    inputPlace.appendChild(buildLoginForm())
     buttonPlace.appendChild(linkButtons.register())
     return content
 }
