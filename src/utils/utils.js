939f2d48e38d0705e53f31b8484762506157ffe6
Verisana <account@0112.pro>
2021-08-23 16:45:28 +0500

    feat: create convertStyles2Strings to map styles list to style string

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,3 +5,17 @@ export const string2DomElement = (toParse) => {
     const parsed = new DOMParser().parseFromString(toParse, "text/html")
     return parsed.body.firstChild
 }
+
+// Преобразуем список со стилями в строку для проброса в шаблон
+export const convertStyles2Strings = (classMappings, classList) => {
+    return classList === undefined ? "" : classList
+        .map((el) => {
+            const mapping = classMappings[el]
+            if (mapping === undefined)
+                throw new Error(
+                    `Can not map class to converted style class. Check arguments`
+                )
+            return mapping
+        })
+        .reduce((acc, classValue) => acc + ` ${classValue}`)
+}
