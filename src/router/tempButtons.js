f7ff669f43a57086a7cd121b17104ed135a77d80
Verisana <account@0112.pro>
2021-08-23 23:39:40 +0500

    feat: update fields to match new button namings

--- src/router/tempButtons.js
+++ src/router/tempButtons.js
@@ -7,8 +7,8 @@ import { switchContent } from "./router.js"
 const linkButtons = {
     home: () => {
         const buttonElement = button({
-            buttonName: "Home",
-            classList: ["button__submit_red"]
+            text: "Home",
+            class_: ["button__submit_red"]
         })
         buttonElement.addEventListener("click", () => {
             switchContent("home")
@@ -16,35 +16,35 @@ const linkButtons = {
         return buttonElement
     },
     login: () => {
-        const buttonElement = button({ buttonName: "Login" })
+        const buttonElement = button({ text: "Login" })
         buttonElement.addEventListener("click", () => {
             switchContent("login")
         })
         return buttonElement
     },
     register: () => {
-        const buttonElement = button({ buttonName: "Register" })
+        const buttonElement = button({ text: "Register" })
         buttonElement.addEventListener("click", () => {
             switchContent("register")
         })
         return buttonElement
     },
     error: () => {
-        const buttonElement = button({ buttonName: "Error" })
+        const buttonElement = button({ text: "Error" })
         buttonElement.addEventListener("click", () => {
             switchContent("error")
         })
         return buttonElement
     },
     serverError: () => {
-        const buttonElement = button({ buttonName: "Server error" })
+        const buttonElement = button({ text: "Server error" })
         buttonElement.addEventListener("click", () => {
             switchContent("serverError")
         })
         return buttonElement
     },
     profileSettings: () => {
-        const buttonElement = button({ buttonName: "Settings" })
+        const buttonElement = button({ text: "Settings" })
         buttonElement.addEventListener("click", () => {
             switchContent("profileSettings")
         })
