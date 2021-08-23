d27eeed3726738c42f4de3c7a170e488ee0b9fb7
Verisana <account@0112.pro>
2021-08-23 18:14:42 +0500

    feat: update footer to function and rerender on each call

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,10 +2,12 @@ import footerTemplate from "./footer.hbs"
 import { string2DomElement } from "../../utils/utils.js"
 import { linkButtons } from "../../router/tempButtons.js"
 
-const footer = string2DomElement(footerTemplate())
-
-for (const [, button] of Object.entries(linkButtons)) {
-    footer.appendChild(button)
+const footer = () => {
+    const element = string2DomElement(footerTemplate())
+    for (const [, button] of Object.entries(linkButtons)) {
+        element.appendChild(button())
+    }
+    return element
 }
 
 export { footer }
