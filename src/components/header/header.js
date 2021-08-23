61916def78da8fc8f6cb2a1e2baf41e28844a08d
Verisana <account@0112.pro>
2021-08-23 18:14:22 +0500

    feat: update header to function and return new instance of element

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,8 @@
 import headerTemplate from "./header.hbs"
 import { string2DomElement } from "../../utils/utils.js"
 
-const header = string2DomElement(headerTemplate())
+const header = () => {
+    return string2DomElement(headerTemplate())
+}
 
 export { header }
