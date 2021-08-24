995626d1bc4a3b6288bc81e1fa94f502cfba431f
Verisana <account@0112.pro>
2021-08-24 19:52:24 +0500

    feat: create working header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,44 @@
 import headerTemplate from "./header.hbs"
 import { string2DomElement } from "../../utils/utils.js"
+import { linkButtons } from "../../router/tempButtons.js"
+import { button } from "../button/index.js"
 
-const header = () => {
-    return string2DomElement(headerTemplate())
+export const placeholders = {
+    menuButton: "header-menu-button",
+    settingsButton: "header-menu-settings",
+    buttonToLogin: "header-button-to-login",
+    buttonToRegister: "header-button-to-register"
 }
 
-export { header }
+export const header = (isLogged) => {
+    const params = {
+        isLogged,
+        ...placeholders
+    }
+    const header_ = string2DomElement(headerTemplate(params))
+
+    if (isLogged) {
+        const menuButtonDiv = header_.querySelector(
+            `#${placeholders.menuButton}`
+        )
+        menuButtonDiv.appendChild(button({ imgSrc: "menu_white_48dp.svg" }))
+        const settingsButtonDiv = header_.querySelector(
+            `#${placeholders.settingsButton}`
+        )
+        settingsButtonDiv.appendChild(
+            linkButtons.profileSettings({ imgSrc: "settings_white_48dp.svg" })
+        )
+    } else {
+        const buttonToLoginDiv = header_.querySelector(
+            `#${placeholders.buttonToLogin}`
+        )
+        buttonToLoginDiv.appendChild(linkButtons.login({ text: "Залогиниться" }))
+        const buttonToRegisterDiv = header_.querySelector(
+            `#${placeholders.buttonToRegister}`
+        )
+        buttonToRegisterDiv.appendChild(
+            linkButtons.register({ text: "Зарегистрироваться" })
+        )
+    }
+    return header_
+}
