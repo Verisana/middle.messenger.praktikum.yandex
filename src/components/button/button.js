d2602d2f6adcb459a71b38ef36a91dcb18a5c20a
Verisana <account@0112.pro>
2021-08-24 19:50:44 +0500

    feat: ass new arguments: imgSrc and type for button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,11 +2,12 @@ import styles from "./button.css"
 import buttonTemplate from "./button.hbs"
 import { string2DomElement, convertStyles2Strings } from "../../utils/utils.js"
 
-const button = ({ text, class_, type_ } = {}) => {
+const button = ({ text, class_, type_, imgSrc } = {}) => {
     const params = {
         class_: convertStyles2Strings(styles, class_),
         text,
-        type_
+        type_,
+        imgSrc
     }
     return string2DomElement(buttonTemplate(params))
 }
