52efe3c4ac61540e41f906521c53735af51ac130
Verisana <account@0112.pro>
2021-08-24 19:51:03 +0500

    feat: set proper footer text

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,6 +4,9 @@ import { linkButtons } from "../../router/tempButtons.js"
 
 const footer = () => {
     const element = string2DomElement(footerTemplate())
+    const paragraph = document.createElement("p")
+    paragraph.textContent = "Служебные кнопки для проверки страниц задания"
+    element.appendChild(paragraph)
     for (const [, button] of Object.entries(linkButtons)) {
         element.appendChild(button())
     }
