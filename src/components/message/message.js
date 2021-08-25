a68bef23a813d1440c2e9e58edc28de8ae0833cc
Verisana <account@0112.pro>
2021-08-25 14:30:52 +0500

    feat: create message component

---
+++ src/components/message/message.js
@@ -0,0 +1,15 @@
+import styles from "./message.css"
+import messageTemplate from "./message.hbs"
+import { string2DomElement, convertStyles2Strings } from "../../utils/utils.js"
+
+export const message = ({ messageId, text, sender, timeMachine, timeHuman, classList } = {}) => {
+    const params = {
+        class_: convertStyles2Strings(styles, classList),
+        timeMachine,
+        timeHuman,
+        messageId,
+        sender,
+        text
+    }
+    return string2DomElement(messageTemplate(params))
+}
