5a6e85bb8f173eaf65a679190b985ec0e13445a3
Verisana <account@0112.pro>
2021-08-25 21:26:15 +0500

    feat: add default btn style to button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,8 +3,9 @@ import buttonTemplate from "./button.hbs"
 import { string2DomElement, convertStyles2Strings } from "../../utils/utils.js"
 
 const button = ({ text, class_, type_, imgSrc } = {}) => {
+    const newClass_ = class_ === undefined ? ["btn"] : ["btn", ...class_]
     const params = {
-        class_: convertStyles2Strings(styles, class_),
+        class_: convertStyles2Strings(styles, newClass_),
         text,
         type_,
         imgSrc
