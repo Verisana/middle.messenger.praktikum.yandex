674b768f82ce7abf63f8201b58ee50bffc0ee9fc
Verisana <account@0112.pro>
2021-08-25 21:28:10 +0500

    feat: update footer.js to add new styles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,15 @@
+import "./footer.css"
+import layoutStyles from "../../layout/layout.css"
 import footerTemplate from "./footer.hbs"
-import { string2DomElement } from "../../utils/utils.js"
+import { string2DomElement} from "../../utils/utils.js"
 import { linkButtons } from "../../router/tempButtons.js"
 
 const footer = () => {
-    const element = string2DomElement(footerTemplate())
+    const params = {
+        contentClass: layoutStyles.content
+    }
+
+    const element = string2DomElement(footerTemplate(params))
     const paragraph = document.createElement("p")
     paragraph.textContent = "Служебные кнопки для проверки страниц задания"
     element.appendChild(paragraph)
