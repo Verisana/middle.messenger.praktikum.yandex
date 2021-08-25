dbc6de063a0f6a88ee5f335a98cee609cd960de4
Verisana <account@0112.pro>
2021-08-25 14:31:53 +0500

    feat: create home page and fill with datas

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,167 @@
-import classes from "./home.css"
+import styles from "./home.css"
 import homeTemplate from "./home.hbs"
-import { string2DomElement } from "../../utils/utils.js"
+import { string2DomElement, onSubmitMock } from "../../utils/utils.js"
+import { inputField } from "../../components/inputField/index.js"
+import { submitForm } from "../../components/submitForm/index.js"
+import { button } from "../../components/button/index.js"
+import { message } from "../../components/message/index.js"
+import { chatSideBar } from "../../components/chatSideBar/index.js"
+import { linkButtons } from "../../router/tempButtons.js"
+import { isLogged } from "../../consts.js"
 
-const homeContent = () => {
-    const content = string2DomElement(homeTemplate())
-    return content
+const chatSideBarList = [
+    chatSideBar({
+        messageRead: false,
+        contactName: "Contact",
+        contactId: "1",
+        messageElement: message({
+            sender: "Owner",
+            text: "Текст сообщения",
+            timeMachine: "12:45:01.235",
+            timeHuman: "12:45",
+            messageId: "10"
+        })
+    }),
+    chatSideBar({
+        messageRead: true,
+        contactName: "Contact_2",
+        contactId: "2",
+        messageElement: message({
+            sender: "Owner",
+            text: "Текст сообщения 2",
+            timeMachine: "12:45:01.235",
+            timeHuman: "12:45",
+            messageId: "11"
+        })
+    }),
+    chatSideBar({
+        messageRead: true,
+        contactName: "Contact_3",
+        contactId: "3",
+        messageElement: message({
+            sender: "Companion",
+            text: "Текст сообщения 3",
+            timeMachine: "12:45:01.235",
+            timeHuman: "12:45",
+            messageId: "12"
+        })
+    })
+]
+
+const messages = [
+    message({
+        sender: "Owner",
+        text: "Привет!",
+        timeMachine: "12:45:01.235",
+        timeHuman: "12:45",
+        messageId: "1"
+    }),
+    message({
+        sender: "Companion",
+        text: "Прив",
+        timeMachine: "12:46:02.23",
+        timeHuman: "12:46",
+        messageId: "2"
+    }),
+    message({
+        sender: "Owner",
+        text: "Ты будешь завтра на вебинаре?",
+        timeMachine: "12:47:01:68",
+        timeHuman: "12:47",
+        messageId: "3"
+    }),
+    message({
+        sender: "Companion",
+        text: "Да, планирую",
+        timeMachine: "12:48:03.39",
+        timeHuman: "12:48",
+        messageId: "4"
+    }),
+    message({
+        sender: "Companion",
+        text: "А ты?",
+        timeMachine: "12:48:45.355",
+        timeHuman: "12:48",
+        messageId: "5"
+    }),
+    message({
+        sender: "Owner",
+        text: "Ага, тоже",
+        timeMachine: "12:49:25.355",
+        timeHuman: "12:49",
+        messageId: "6"
+    })
+]
+
+const buildMessageInputForm = () => {
+    const inputBuilders = [
+        inputField.bind(null, {
+            input_: {
+                type: "text",
+                placeholder: "Сообщение",
+                name: "message",
+                required: true
+            }
+        })
+    ]
+    const submitBuilder = button.bind(null, {
+        text: "Отправить",
+        type_: "submit"
+    })
+    return submitForm({
+        inputBuilders,
+        submitBuilder,
+        onSubmitFunc: onSubmitMock
+    })
 }
 
-export { homeContent }
+export const placeholders = {
+    chatSideBarPlace: "home-chat-sidebar-list",
+    messagesPlace: "home-chat-messages-list",
+    textInputPlace: "home-input-message-field",
+    buttonToLogin: "home-button-to-login",
+    buttonToRegister: "home-button-to-register"
+}
+
+export const homeContent = () => {
+    const params = {
+        isLogged,
+        ...placeholders
+    }
+    const content = string2DomElement(homeTemplate(params))
+
+    if (isLogged) {
+        const chatSideBarDiv = content.querySelector(
+            `#${placeholders.chatSideBarPlace}`
+        )
+        for (const chatSideBar of chatSideBarList) {
+            chatSideBarDiv.appendChild(chatSideBar)
+        }
+
+        const messagesDiv = content.querySelector(
+            `#${placeholders.messagesPlace}`
+        )
+        for (const message of messages) {
+            messagesDiv.appendChild(message)
+        }
+
+        const textInputDiv = content.querySelector(
+            `#${placeholders.textInputPlace}`
+        )
+        textInputDiv.appendChild(buildMessageInputForm())
+    } else {
+        const buttonToLoginDiv = content.querySelector(
+            `#${placeholders.buttonToLogin}`
+        )
+        buttonToLoginDiv.appendChild(
+            linkButtons.login({ text: "Залогиниться" })
+        )
+        const buttonToRegisterDiv = content.querySelector(
+            `#${placeholders.buttonToRegister}`
+        )
+        buttonToRegisterDiv.appendChild(
+            linkButtons.register({ text: "Зарегистрироваться" })
+        )
+    }
+    return content
+}
