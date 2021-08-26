344e395033195b712ff26ee883ef0f70d62a666a
Verisana <account@0112.pro>
2021-08-26 20:06:12 +0500

    feat: add imgStyle support for button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,13 +2,14 @@ import styles from "./button.css"
 import buttonTemplate from "./button.hbs"
 import { string2DomElement, convertStyles2Strings } from "../../utils/utils.js"
 
-const button = ({ text, class_, type_, imgSrc } = {}) => {
+const button = ({ text, class_, type_, imgSrc, imgStyle } = {}) => {
     const newClass_ = class_ === undefined ? ["btn"] : ["btn", ...class_]
     const params = {
         class_: convertStyles2Strings(styles, newClass_),
         text,
         type_,
-        imgSrc
+        imgSrc,
+        imgStyle: convertStyles2Strings(styles, imgStyle)
     }
     return string2DomElement(buttonTemplate(params))
 }
