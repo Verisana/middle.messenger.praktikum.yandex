6d5b4228c5f2b962373600bafe3e54c25ddf0a5e
Verisana <account@0112.pro>
2021-08-26 20:07:11 +0500

    feat: add more styles to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import "./header.css"
+import styles from "./header.css"
 import headerTemplate from "./header.hbs"
 import { string2DomElement } from "../../utils/utils.js"
 import { linkButtons } from "../../router/tempButtons.js"
@@ -7,12 +7,13 @@ import { isLogged } from "../../consts.js"
 
 export const placeholders = {
     menuButton: "header-menu-button",
-    settingsButton: "header-menu-settings",
+    settingsButton: "header-menu-settings"
 }
 
 export const header = () => {
     const params = {
         isLogged,
+        logoStyles: styles.logo__style_default,
         ...placeholders
     }
     const header_ = string2DomElement(headerTemplate(params))
@@ -21,12 +22,22 @@ export const header = () => {
         const menuButtonDiv = header_.querySelector(
             `#${placeholders.menuButton}`
         )
-        menuButtonDiv.appendChild(button({ imgSrc: "menu_white_48dp.svg" }))
+        menuButtonDiv.appendChild(
+            button({
+                imgSrc: "menu_white_48dp.svg",
+                class_: ["btn__navbar_default"],
+                imgStyle: ["btn__image_default"]
+            })
+        )
         const settingsButtonDiv = header_.querySelector(
             `#${placeholders.settingsButton}`
         )
         settingsButtonDiv.appendChild(
-            linkButtons.profileSettings({ imgSrc: "settings_white_48dp.svg" })
+            linkButtons.profileSettings({
+                imgSrc: "settings_white_48dp.svg",
+                class_: ["btn__navbar_default"],
+                imgStyle: ["btn__image_default"]
+            })
         )
     }
 
