209921311c973e6804bcf1f249fc2346ae631bdb
Verisana <account@0112.pro>
2021-09-06 16:10:42 +0500

    refactor: make first argument unnamed

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, "..", "dist")))
 
-app.get("/", (req, res) => {
+app.get("/", (_0, res) => {
     res.sendFile(path.join(__dirname, "..", "dist", "index.html"))
 })
 
