5e92a15615546efffd72dde87fcaf3f9f1c9f061
Verisana <account@0112.pro>
2021-09-07 15:38:49 +0500

    feat: create types for Block class

---
+++ src/block/types.ts
@@ -0,0 +1,25 @@
+export type Events = Record<string, (event: Event) => void>
+
+export interface IBlockSettings {
+    withInternalID: boolean
+}
+
+export type Props = {
+    __id?: string
+    [key: string]: unknown
+}
+
+export type TemplateCreator = (props: Props) => string
+
+export type BlockParams = {
+    templateFunc: TemplateCreator
+    events?: Events
+    settings?: IBlockSettings
+    props?: Props
+}
+
+export interface IMeta {
+    tagName: string
+    params: BlockParams
+    id?: string
+}
