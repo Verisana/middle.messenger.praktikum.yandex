7b710de2058f936f3668f5b06d2b02c7bcb4cdf6
Verisana <account@0112.pro>
2021-09-08 17:46:44 +0500

    feat: create new Button class from block

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -1,24 +1,22 @@
 import styles from "./button.css"
 import buttonTemplate from "./button.hbs"
-import { string2DomElement, convertStyles2Strings } from "../../utils/utils"
-import { IButton } from "./types"
+import { convertStyles2Strings } from "../../utils/utils"
+import { IButtonParams } from "./types"
+import { Block } from "../../block"
 
-export const button = ({
-    text,
-    class_,
-    type_ = "button",
-    imgSrc,
-    imgStyle
-}: IButton): Element => {
-    const params = {
-        class_: convertStyles2Strings([styles], "btn", class_),
-        text,
-        type_,
-        imgSrc,
-        imgStyle:
-            imgStyle !== undefined
-                ? convertStyles2Strings([styles], imgStyle)
+export class Button extends Block {
+    constructor(params: IButtonParams) {
+        const { props } = params
+        props.type_ = props.type_ === undefined ? "button" : props.type_
+        props.class_ = convertStyles2Strings([styles], "btn", props.class_)
+        props.imgStyle =
+            props.imgStyle === undefined
+                ? convertStyles2Strings([styles], props.imgStyle)
                 : undefined
+        super("button", params)
     }
-    return string2DomElement(buttonTemplate(params))
+
+    render(): string {
+        return buttonTemplate(this.props)
+    }
 }
