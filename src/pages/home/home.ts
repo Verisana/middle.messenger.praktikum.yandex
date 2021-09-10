b01a11b6c59ec95951af7b9198511faa7c247113
Verisana <account@0112.pro>
2021-09-10 16:17:39 +0500

    feat: use new component modules in home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,8 @@ import {
 } from "../../utils/utils"
 import { inputField } from "../../components/inputField"
 import { submitForm } from "../../components/submitForm"
-import { message } from "../../components/message"
-import { chatSideBar } from "../../components/chatSideBar"
+import { Message } from "../../components/message"
+import { ChatSideBar } from "../../components/chatSideBar"
 import { linkButtons } from "../../router/tempButtons"
 import { isLogged } from "../../consts"
 import { loginContent } from "../auth/login"
@@ -17,92 +17,116 @@ import { registerContent } from "../auth/register"
 import { Button } from "../../components/button"
 
 const chatSideBarList = [
-    chatSideBar({
-        messageIsRead: false,
-        contactName: "Contact",
-        contactId: 1,
-        messageElement: message({
-            sender: "owner",
-            text: "Текст сообщения",
-            timeMachine: "12:45:01.235",
-            timeHuman: "12:45",
-            messageId: 10
-        })
+    new ChatSideBar({
+        props: {
+            messageIsRead: false,
+            contactName: "Contact",
+            contactId: 1,
+            Message: new Message({
+                props: {
+                    sender: "owner",
+                    text: "Текст сообщения",
+                    timeMachine: "12:45:01.235",
+                    timeHuman: "12:45",
+                    messageId: 10
+                }
+            })
+        }
     }),
-    chatSideBar({
-        messageIsRead: true,
-        contactName: "Contact_2",
-        contactId: 2,
-        messageElement: message({
-            sender: "owner",
-            text: "Текст сообщения 2",
-            timeMachine: "12:45:01.235",
-            timeHuman: "12:45",
-            messageId: 11
-        })
+    new ChatSideBar({
+        props: {
+            messageIsRead: true,
+            contactName: "Contact_2",
+            contactId: 2,
+            Message: new Message({
+                props: {
+                    sender: "owner",
+                    text: "Текст сообщения 2",
+                    timeMachine: "12:45:01.235",
+                    timeHuman: "12:45",
+                    messageId: 11
+                }
+            })
+        }
     }),
-    chatSideBar({
-        messageIsRead: true,
-        contactName: "Contact_3",
-        contactId: 3,
-        messageElement: message({
-            sender: "companion",
-            text: "Текст сообщения 3",
-            timeMachine: "12:45:01.235",
-            timeHuman: "12:45",
-            messageId: 12
-        })
+    new ChatSideBar({
+        props: {
+            messageIsRead: true,
+            contactName: "Contact_3",
+            contactId: 3,
+            Message: new Message({
+                props: {
+                    sender: "companion",
+                    text: "Текст сообщения 3",
+                    timeMachine: "12:45:01.235",
+                    timeHuman: "12:45",
+                    messageId: 12
+                }
+            })
+        }
     })
 ]
 
 const messages = [
-    message({
-        sender: "owner",
-        text: "Привет!",
-        timeMachine: "12:45:01.235",
-        timeHuman: "12:45",
-        messageId: 1,
-        classList: ["message_right"]
+    new Message({
+        props: {
+            sender: "owner",
+            text: "Привет!",
+            timeMachine: "12:45:01.235",
+            timeHuman: "12:45",
+            messageId: 1,
+            rootClass: ["message_right"]
+        }
     }),
-    message({
-        sender: "companion",
-        text: "Прив",
-        timeMachine: "12:46:02.23",
-        timeHuman: "12:46",
-        messageId: 2,
-        classList: ["message_left"]
+    new Message({
+        props: {
+            sender: "companion",
+            text: "Прив",
+            timeMachine: "12:46:02.23",
+            timeHuman: "12:46",
+            messageId: 2,
+            rootClass: ["message_left"]
+        }
     }),
-    message({
-        sender: "owner",
-        text: "Ты будешь завтра на вебинаре?",
-        timeMachine: "12:47:01:68",
-        timeHuman: "12:47",
-        messageId: 3,
-        classList: ["message_right"]
+    new Message({
+        props: {
+            sender: "owner",
+            text: "Ты будешь завтра на вебинаре?",
+            timeMachine: "12:47:01:68",
+            timeHuman: "12:47",
+            messageId: 3,
+            rootClass: ["message_right"]
+        }
     }),
-    message({
-        sender: "companion",
-        text: "Да, планирую",
-        timeMachine: "12:48:03.39",
-        timeHuman: "12:48",
-        messageId: 4,
-        classList: ["message_left"]
+    new Message({
+        props: {
+            sender: "companion",
+            text: "Да, планирую",
+            timeMachine: "12:48:03.39",
+            timeHuman: "12:48",
+            messageId: 4,
+            rootClass: ["message_left"]
+        }
     }),
-    message({
-        sender: "companion",
-        text: "А ты?",
-        timeMachine: "12:48:45.355",
-        timeHuman: "12:48",
-        messageId: 5,
-        classList: ["message_left"]
+    new Message({
+        props: {
+            sender: "companion",
+            text: "А ты?",
+            timeMachine: "12:48:45.355",
+            timeHuman: "12:48",
+            messageId: 5,
+            rootClass: ["message_left"]
+        }
     }),
-    message({
-        sender: "owner",
-        text: "Ага, тоже",
-        timeMachine: "12:49:25.355",
-        timeHuman: "12:49",
-        messageId: 6,
-        classList: ["message_right"]
+    new Message({
+        props: {
+            sender: "owner",
+            text: "Ага, тоже",
+            timeMachine: "12:49:25.355",
+            timeHuman: "12:49",
+            messageId: 6,
+            rootClass: ["message_right"]
+        }
     })
 ]
 
@@ -159,7 +183,7 @@ export const homeContent = () => {
             placeholders.chatSideBarPlace
         )
         for (const chatSideBarElement of chatSideBarList) {
-            chatSideBarDiv.appendChild(chatSideBarElement)
+            chatSideBarDiv.appendChild(chatSideBarElement.getContent())
         }
 
         const messagesDiv = selectPlaceholder(
@@ -167,7 +191,7 @@ export const homeContent = () => {
             placeholders.messagesPlace
         )
         for (const messageElement of messages) {
-            messagesDiv.appendChild(messageElement)
+            messagesDiv.appendChild(messageElement.getContent())
         }
 
         const textInputDiv = selectPlaceholder(
