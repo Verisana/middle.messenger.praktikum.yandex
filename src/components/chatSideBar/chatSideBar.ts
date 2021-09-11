b9a8a4227ca63d704eff434d4997059d33e91f56
Verisana <account@0112.pro>
2021-09-11 18:07:25 +0500

    style: remove redundant code

--- src/components/chatSideBar/chatSideBar.ts
+++ src/components/chatSideBar/chatSideBar.ts
@@ -30,37 +30,3 @@ export class ChatSideBar extends Block {
         return compile2Dom(chatSideBarTemplate, this.props)
     }
 }
-
-// export const placeholders = {
-//     messagePlace: "chat-sidebar-contact-last-message"
-// }
-
-// export const chatSideBar = ({
-//     messageElement,
-//     classList,
-//     messageIsRead,
-//     avatarSrc,
-//     contactName,
-//     contactId
-// }: IChatSideBar) => {
-//     const params = {
-//         class_: convertStyles2Strings([styles], "chatSideBar_main", classList),
-//         avatarSrc: avatarSrc === undefined ? defaultAvatar : avatarSrc,
-//         messageIsRead,
-//         contactName,
-//         contactId,
-//         imgStyles: convertStyles2Strings([layoutStyles], "img__avatar_small"),
-//         ...placeholders
-//     }
-//     const element = string2DomElement(chatSideBarTemplate(params))
-//     const messagePlaceDiv = selectPlaceholder(
-//         element,
-//         placeholders.messagePlace
-//     )
-//     const { textContent } = messageElement
-//     messageElement.textContent =
-//         textContent !== null ? textContent.slice(0, maxMessageLength) : null
-
-//     messagePlaceDiv.appendChild(messageElement)
-//     return element
-// }
