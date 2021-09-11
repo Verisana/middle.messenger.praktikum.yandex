620bc82a00308da2bb35cacd0baabfc8a0c64cae
Verisana <account@0112.pro>
2021-09-11 14:36:03 +0500

    feat: export HomePage and ErrorPage properly

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,15 +1,15 @@
-import { homeContent } from "./home"
-import { errorContent } from "./errors/404"
+import { HomePage } from "./home"
+import { ErrorPage } from "./errors/404"
 import { serverErrorContent } from "./errors/500"
 import { loginContent } from "./auth/login"
 import { registerContent } from "./auth/register"
 import { settingsContent } from "./settings"
 
 export const pages = {
-    homeContent,
+    home: () => new HomePage(),
     loginContent,
     registerContent,
     settingsContent,
-    errorContent,
+    error: () => new ErrorPage(),
     serverErrorContent
 }
