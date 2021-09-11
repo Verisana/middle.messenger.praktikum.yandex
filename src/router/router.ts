74e6a7163a4ff93f0d06c0834a4e3d7b854c56f0
Verisana <account@0112.pro>
2021-09-11 14:37:48 +0500

    feat: update switchcontent in Router

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,8 +1,12 @@
-import { renderContent } from "../layout"
+import { Block } from "../block"
+import { ILayoutProps, layout } from "../layout"
+import { render } from "../utils/utils"
 
 // Пока мы не дошли до роутинга, чтобы не плодить лишние html файлы, которые
 // меня печалят, решил использовать эту заглушку, чтобы отображать страницы
 
-export const switchContent = (content: () => Element) => {
-    renderContent(content)
+export const switchContent = (content: () => Block) => {
+    const props = layout.props as ILayoutProps
+    props.Content = content()
+    // render("#App", layout)
 }
