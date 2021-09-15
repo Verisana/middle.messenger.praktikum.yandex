84854e6f345b0803901124475dfa89e2f81c23de
Verisana <account@0112.pro>
2021-09-15 19:09:53 +0500

    feat: hide label in input if input has value

--- src/components/inputConstruct/inputConstruct.ts
+++ src/components/inputConstruct/inputConstruct.ts
@@ -1,8 +1,30 @@
 import styles from "./inputConstruct.css"
 import inputConstructTemplate from "./inputConstruct.hbs"
-import { convertStyles2Strings, compile2Dom } from "../../utils/utils"
+import {
+    convertStyles2Strings,
+    compile2Dom,
+    pushEvent
+} from "../../utils/utils"
 import { IInputConstructParams } from "./types"
 import { Block } from "../../block"
+import { IInputFieldProps } from "../inputField"
+
+const hideLabelIfNoValue = (event: Event) => {
+    if (event.target instanceof HTMLInputElement) {
+        const label = event.target.parentElement?.querySelector("label")
+        if (label !== null && label !== undefined) {
+            console.log(event.target.value.length)
+            if (event.target.value.length > 0) {
+                label.style.display = "none"
+            } else {
+                label.style.display = "block"
+            }
+        }
+        return
+    }
+
+    throw new Error("Must bu used only on HTMLInputElement")
+}
 
 export class InputConstruct extends Block {
     constructor(params: IInputConstructParams) {
@@ -21,6 +43,14 @@ export class InputConstruct extends Block {
                       text: props.label.text,
                       class: convertStyles2Strings([styles], props.label.class)
                   }
+        const inputFieldProps = props.InputField.props as IInputFieldProps
+        const inputEvents =
+            inputFieldProps.events === undefined ? {} : inputFieldProps.events
+
+        pushEvent(inputEvents, "keyup", hideLabelIfNoValue)
+
+        // Обязательно нужно переприсвоить, чтобы затригерились изменения
+        inputFieldProps.events = inputEvents
         super(params)
     }
 
