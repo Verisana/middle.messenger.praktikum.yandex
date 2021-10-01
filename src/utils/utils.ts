7d9480656cc10c7b0cf975e5406d22eaf821aa6a
Verisana <account@0112.pro>
2021-10-01 19:12:41 +0500

    feat: create isEqual utils to utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -72,87 +72,6 @@ export const capitalizeFirstSymbol = (text: string): string | undefined => {
     return text.charAt(0).toUpperCase() + text.slice(1)
 }
 
-export const selectPlaceholder = (element: Element, id_: string): Element => {
-    const placeholder = element.querySelector(`#${id_}`)
-    if (placeholder === null)
-        throw new Error(`Element with ${id_} can not be found`)
-    return placeholder
-}
-
-export const render = (query: string, block: Block): Element => {
-    const root = document.querySelector(query)
-    if (root === null) throw new Error("Check your query")
-
-    if (block.content !== null) {
-        root.replaceWith(block.content)
-    } else {
-        throw new Error("Can not render. No content available")
-    }
-    return root
-}
-
-const fillComponentId = (
-    components: Record<string, Block>,
-    key: string,
-    value: unknown,
-    context: Props,
-    isValueArray: boolean
-) => {
-    if (value instanceof Block) {
-        const id = uuidv4()
-
-        const mockDomString = `<div data-content-id="${id}"></div>` // делаем заглушку
-        components[id] = value // сохраняем компонент
-        if (isValueArray) {
-            if (context[key] === undefined) {
-                context[key] = []
-            }
-            // TypeScript не понимал, что у меня тут 100% будет массив, поэтому
-            // пришлось вручную пробросить тип
-            const contextValue = context[key] as string[]
-            contextValue.push(mockDomString)
-        } else {
-            context[key] = mockDomString
-        }
-    } else {
-        context[key] = value
-    }
-}
-
-export const compile2Dom = (
-    templateFunc: (context: Props) => string,
-    proxyContext: Props
-): HTMLElement => {
-    const fragment = document.createElement("template")
-    const components: Record<string, Block> = {}
-
-    // Создаем дубликат массива, чтобы не триггерить CDU, когда перезаписываем
-    // в пропсах mock значения
-    const context: Props = {}
-
-    for (const [key, value] of Object.entries(proxyContext)) {
-        if (Array.isArray(value)) {
-            for (const arrayValue of value) {
-                fillComponentId(components, key, arrayValue, context, true)
-            }
-        } else fillComponentId(components, key, value, context, false)
-    }
-    fragment.innerHTML = templateFunc(context)
-    for (const [id, component] of Object.entries(components)) {
-        const stub = fragment.content.querySelector(`[data-content-id="${id}"]`)
-
-        // Здесь разрешено получать null, потому что шаблон может рендерить места
-        // В зависимости от условий. Тогда получается, что у нас может быть компонент
-        // но места под него нет в текущем состоянии.
-        if (stub !== null) {
-            if (component.content === null)
-                throw new Error("Content can not bu nulled")
-            stub.replaceWith(component.content)
-        }
-    }
-    return fragment.content.firstChild as HTMLElement
-}
-
 export const pushEvent = (
     events: Events,
     eventName: string,
@@ -163,4 +82,43 @@ export const pushEvent = (
     } else {
         events[eventName] = [eventListener]
     }
+}
+
+export const isArray = (value: unknown): value is [] => {
+    return Array.isArray(value)
+}
+
+export const isPlainObject = (value: unknown): value is PlainObject => {
+    return (
+        typeof value === "object" &&
+        value !== null &&
+        value.constructor === Object &&
+        Object.prototype.toString.call(value) === "[object Object]"
+    )
+}
+
+export const isArrayOrObject = (value: unknown): value is [] | PlainObject => {
+    return isPlainObject(value) || isArray(value)
+}
+
+export const isEqual = (lhs: PlainObject, rhs: PlainObject): boolean => {
+    // Сравнение количества ключей объектов и массивов
+    if (Object.keys(lhs).length !== Object.keys(rhs).length) {
+        return false
+    }
+
+    for (const [key, value] of Object.entries(lhs)) {
+        const rightValue = rhs[key]
+        if (isArrayOrObject(value) && isArrayOrObject(rightValue)) {
+            if (!isEqual(value, rightValue)) {
+                return false
+            }
+        }
+
+        if (value !== rightValue) {
+            return false
+        }
+    }
+
+    return true
 }
