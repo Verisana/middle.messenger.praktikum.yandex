9236282ccc74ecd0561645430f5d98ecf0f676e4
Verisana <account@0112.pro>
2021-10-02 15:08:32 +0500

    feat: create logo component

---
+++ src/components/logo/logo.ts
@@ -0,0 +1,38 @@
+import styles from "./logo.css"
+import logoTemplate from "./logo.hbs"
+import { compileToDom } from "../../utils/dom_utils"
+import { Block } from "../block"
+import { ILogoParams } from "./types"
+import { appendEvent, convertStyles2Strings } from "../../utils/utils"
+import { urlSlugs } from "../../consts"
+import { routerFactory } from "../../router"
+
+const router = routerFactory()
+
+export class Logo extends Block {
+    constructor(params: ILogoParams) {
+        const { props } = params
+        if (props.rootClass === undefined) {
+            props.rootClass = convertStyles2Strings([styles], "logo")
+        } else {
+            props.rootClass = convertStyles2Strings(
+                [styles],
+                "logo",
+                props.rootClass
+            )
+        }
+        if (props.events === undefined) {
+            props.events = {}
+        }
+        appendEvent(
+            props.events,
+            "click",
+            router.go.bind(router, urlSlugs.home)
+        )
+        super(params)
+    }
+
+    render(): HTMLElement {
+        return compileToDom(logoTemplate, this.props)
+    }
+}
