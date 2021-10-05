739fa29a8e16678937706be19875850a3dd205bf
Verisana <account@0112.pro>
2021-10-05 09:48:10 +0500

    refactor: move queryString types to types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,13 @@
-export type PlainObject<T = unknown> = {
+export type PlainObject<T = any> = {
     [k in string]: T
+}
+
+export type ParamAble = Record<string, string | number | Array<string | number>>
+
+export interface IRequestOptions {
+    method?: string
+    retries?: number
+    data?: ParamAble
+    timeout?: number
+    headers?: Record<string, string>
 }
