5154e6d66fcf3a8b1477a2470367b22142828d32
Verisana <account@0112.pro>
2021-10-05 09:47:40 +0500

    feat: add new util qeuryString to project

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -131,6 +131,37 @@ export function isEmpty(obj: object): boolean {
     )
 }
 
+export function getKey(key: string, parentKey?: string) {
+    return parentKey ? `${parentKey}[${key}]` : key
+}
+
+export function getParams(data: PlainObject | [], parentKey?: string) {
+    const result: [string, string][] = []
+
+    for (const [key, value] of Object.entries(data)) {
+        if (isArrayOrObject(value)) {
+            result.push(...getParams(value, getKey(key, parentKey)))
+        } else {
+            result.push([
+                getKey(key, parentKey),
+                encodeURIComponent(String(value))
+            ])
+        }
+    }
+
+    return result
+}
+
+export function queryString(data: PlainObject) {
+    if (!isPlainObject(data)) {
+        throw new Error("input must be an object")
+    }
+
+    return getParams(data)
+        .map((arr) => arr.join("="))
+        .join("&")
+}
+
 export function appendEvent(
     events: Events,
     eventName: string,
