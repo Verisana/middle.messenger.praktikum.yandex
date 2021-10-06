7eaaecfee44bdba5e5cc3da43f7d4c241f7ee89e
Verisana <account@0112.pro>
2021-10-06 19:03:29 +0500

    feat: create set function in utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -185,3 +185,25 @@ export function merge(lhs: PlainObject, rhs: PlainObject): PlainObject {
     return lhs
 }
 
+export function set(
+    object: PlainObject | unknown,
+    path: string,
+    value: unknown
+): PlainObject | unknown {
+    if (typeof object !== "object" || object === null) {
+        return object
+    }
+
+    if (typeof path !== "string") {
+        throw new Error("path must be string")
+    }
+
+    const result = path.split(".").reduceRight<PlainObject>(
+        (acc, key) => ({
+            [key]: acc
+        }),
+        value as any
+    )
+    return merge(object as PlainObject, result)
+}
+
