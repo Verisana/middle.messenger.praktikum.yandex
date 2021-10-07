30a043c866ef47f2bba72fd5b40ceb3b39fdac02
Verisana <account@0112.pro>
2021-10-07 12:13:50 +0500

    feat: work on auth_controller

--- src/controllers/auth_controller.ts
+++ src/controllers/auth_controller.ts
@@ -1,4 +1,9 @@
 import { AuthAPI, IRegisterRequest } from "../api"
+import { urlSlugs } from "../consts"
+import { routerFactory } from "../router"
+import { store } from "../store"
+
+const router = routerFactory()
 
 class AuthController {
     private api: AuthAPI
@@ -7,12 +12,30 @@ class AuthController {
         this.api = new AuthAPI()
     }
 
-    async register(data: IRegisterRequest) {
+    async register(formData: FormData) {
         try {
-            const response = await this.api.register(data)
-            console.log(response)
+            const data: IRegisterRequest = {
+                first_name: String(formData.get("first_name")),
+                second_name: String(formData.get("second_name")),
+                login: String(formData.get("login")),
+                password: String(formData.get("password")),
+                email: String(formData.get("email")),
+                phone: String(formData.get("phone"))
+            }
+            await this.api.register(data)
+            await this.userRead()
+            router.go(urlSlugs.home)
         } catch (e) {
             console.log(e)
+        }
+    }
+
+    async userRead() {
+        try {
+            const response = await this.api.userRead()
+            store.setValue("user", response.response)
+        } catch (e) {
+            store.setUndefined("user")
         }
     }
 }
