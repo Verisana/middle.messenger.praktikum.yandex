6ead8a6625dc4809f83825c8b3e78e4271e21e61
Verisana <account@0112.pro>
2021-10-07 19:11:24 +0500

    feat: use redirects to shield settings, messenger pages

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { routerFactory } from "./router"
 import { pages } from "./pages"
 import { urlSlugs } from "./consts"
 import { layoutFactory } from "./layout"
-import { store } from "./store"
 import authController from "./controllers/auth_controller"
+import { loginBlockBuilder, loginRedirectBuilder } from "./router/utils"
 
 const router = routerFactory()
 
@@ -13,17 +13,25 @@ async function main() {
 
     router.useRedirect(
         urlSlugs.home,
-        {
-            login: layoutFactory(pages.login),
-            messenger: layoutFactory(pages.messenger)
-        },
-        () => (store.select("user") !== undefined ? "messenger" : "login")
+        loginBlockBuilder(urlSlugs.messenger, "messenger"),
+        loginRedirectBuilder(urlSlugs.messenger)
     )
 
-    router.use(urlSlugs.register, layoutFactory(pages.register))
     router.use(urlSlugs.login, layoutFactory(pages.login))
-    router.use(urlSlugs.settings, layoutFactory(pages.profileSettings))
-    router.use(urlSlugs.messenger, layoutFactory(pages.messenger))
+    router.use(urlSlugs.register, layoutFactory(pages.register))
+
+    router.useRedirect(
+        urlSlugs.settings,
+        loginBlockBuilder(urlSlugs.settings, "profileSettings"),
+        loginRedirectBuilder(urlSlugs.settings)
+    )
+
+    router.useRedirect(
+        urlSlugs.messenger,
+        loginBlockBuilder(urlSlugs.messenger, "messenger"),
+        loginRedirectBuilder(urlSlugs.messenger)
+    )
+
     router.use(urlSlugs.serverError, layoutFactory(pages.serverError))
     router.useError(urlSlugs.error, layoutFactory(pages.error))
 
