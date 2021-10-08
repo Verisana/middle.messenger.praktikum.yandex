a2362d4de5bb1e17f818849e83abefa03e02f4d7
Verisana <account@0112.pro>
2021-10-08 18:38:17 +0500

    feat: add buttons for chat creating and removing in sideBar

--- src/modules/sideChatBar/types.ts
+++ src/modules/sideChatBar/types.ts
@@ -1,9 +1,12 @@
 import { BlockParams, Props } from "../../components/block"
+import { Button } from "../../components/button"
 import { SearchBar } from "../searchBar"
 import { SideChat } from "../sideChat/sideChat"
 
 export interface ISideChatBarProps extends Props {
     SideChats: SideChat[]
+    ChatCreateButton: Button
+    ChatDeleteButton: Button
     rootClass?: string | string[]
     SearchBar?: SearchBar
 }
