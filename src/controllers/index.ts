23665b8a9fc631c92fec0a57ee15ade91a73470e
Verisana <account@0112.pro>
2021-10-08 18:37:46 +0500

    feat: export chatsController

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export { default as usersController } from "./users_controller"
 export { default as authController } from "./auth_controller"
+export { default as chatsController } from "./chats_controller"
 export { submitControllerBuilder } from "./utils"
