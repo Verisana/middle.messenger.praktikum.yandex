8dcbc5e52891494600a0fb452defe82de0c6e984
Verisana <account@0112.pro>
2021-10-08 09:34:06 +0500

    feat: use value in input field

--- src/components/inputField/types.ts
+++ src/components/inputField/types.ts
@@ -7,6 +7,7 @@ export interface IInputFieldProps extends Props {
     pattern?: string
     placeholder?: string
     name?: string
+    value?: string
 }
 
 export interface IInputFieldParams extends BlockParams {
