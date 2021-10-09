7fb258ff7571390b45c9954da3e74e9c76dd0608
Verisana <account@0112.pro>
2021-10-09 16:52:03 +0500

    feat: create dedicated methods for user and chats setting

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
+import { UserData } from "./api"
+import { IChatsResponse } from "./api/types"
 import { BlockEvents } from "./consts"
 import { IStoreData } from "./types"
 import { EventBus } from "./utils/event_bus"
 import { PlainObject } from "./utils/types"
-import { get, set } from "./utils/utils"
+import { get, normalizeAvatar, set } from "./utils/utils"
 
 export class Store {
     static EVENTS = {
@@ -35,6 +37,29 @@ export class Store {
 
     select(path: string): PlainObject | undefined {
         return get(this.data, path, false)
+    }
+
+    setUser(user: UserData): boolean {
+        user.avatar = normalizeAvatar(user.avatar)
+        return this.setValue("user", user)
+    }
+
+    setChats(chats: IChatsResponse[]): boolean {
+        for (const chat of chats) {
+            chat.avatar = normalizeAvatar(chat.avatar)
+            if (chat.last_message !== null) {
+                chat.last_message.user.avatar = normalizeAvatar(
+                    chat.last_message.user.avatar
+                )
+                const date = new Date(chat.last_message.time)
+                chat.last_message.timeHuman = Intl.DateTimeFormat("ru-RU", {
+                    hour: "numeric",
+                    minute: "numeric"
+                }).format(date)
+            }
+        }
+
+        return this.setValue("chats", chats)
     }
 
     setValue(path: string, value: unknown): boolean {
