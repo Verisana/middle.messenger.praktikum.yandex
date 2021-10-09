7c12a5584881e570f5faf7c9d88a204d1f6fabe9
Verisana <account@0112.pro>
2021-10-09 17:04:45 +0500

    feat: update message names in messenger

--- src/pages/messenger/messenger.ts
+++ src/pages/messenger/messenger.ts
@@ -2,9 +2,7 @@ import { Block } from "../../components/block"
 import styles from "./messenger.css"
 import messengerTemplate from "./messenger.hbs"
 import { compileToDom } from "../../utils/dom_utils"
-import { SideChat } from "../../modules/sideChat"
 import { Message } from "../../components/message"
-import { maxMessageLength } from "../../consts"
 import { TimeInfo } from "../../components/timeInfo"
 import { convertStylesToStrings, onSubmitMock } from "../../utils/utils"
 import { SideChatBar } from "../../modules/sideChatBar"
@@ -16,87 +14,12 @@ import { SearchBar } from "../../modules/searchBar"
 import { InputField } from "../../components/inputField"
 import { chatsController } from "../../controllers"
 
-const sideChatList = [
-    new SideChat({
-        props: {
-            messageIsRead: false,
-            contactName: "Contact",
-            contactId: 1,
-            Message: new Message({
-                settings: { maxTextLength: maxMessageLength },
-                props: {
-                    sender: "owner",
-                    text: "Текст сообщения",
-                    messageId: 10,
-                    rootClass: ["message__sidebar"]
-                }
-            }),
-            Time: new TimeInfo({
-                props: {
-                    timeMachine: "12:45:01.235",
-                    timeHuman: "12:45",
-                    rootClass: ["time-info__chat-side-bar"]
-                }
-            })
-        }
-    }),
-    new SideChat({
-        props: {
-            rootClass: ["side-chat_select"],
-            messageIsRead: true,
-            contactName: "Contact_2",
-            contactId: 2,
-            Message: new Message({
-                settings: { maxTextLength: maxMessageLength },
-                props: {
-                    sender: "owner",
-                    text: "Текст сообщения 2",
-                    messageId: 11,
-                    rootClass: ["message__sidebar"]
-                }
-            }),
-            Time: new TimeInfo({
-                props: {
-                    timeMachine: "12:45:01.235",
-                    timeHuman: "12:45",
-                    rootClass: ["time-info__chat-side-bar"]
-                }
-            })
-        }
-    }),
-    new SideChat({
-        props: {
-            messageIsRead: true,
-            contactName: "Contact_3",
-            contactId: 3,
-            Message: new Message({
-                settings: { maxTextLength: maxMessageLength },
-                props: {
-                    sender: "companion",
-                    text: "Текст сообщения 3",
-                    timeMachine: "12:45:01.235",
-                    timeHuman: "12:45",
-                    messageId: 12,
-                    rootClass: ["message__sidebar"]
-                }
-            }),
-            Time: new TimeInfo({
-                props: {
-                    timeMachine: "12:45:01.235",
-                    timeHuman: "12:45",
-                    rootClass: ["time-info__chat-side-bar"]
-                }
-            })
-        }
-    })
-]
-
 const messages = [
     new Message({
         props: {
-            sender: "owner",
+            senderName: "owner",
             text: "Привет!",
-            messageId: 1,
+            senderId: 1,
             rootClass: ["message__open", "message__open_right"],
             Time: new TimeInfo({
                 props: {
@@ -109,9 +32,9 @@ const messages = [
     }),
     new Message({
         props: {
-            sender: "companion",
+            senderName: "companion",
             text: "Прив",
-            messageId: 2,
+            senderId: 2,
             rootClass: ["message__open", "message__open_left"],
             Time: new TimeInfo({
                 props: {
@@ -124,9 +47,9 @@ const messages = [
     }),
     new Message({
         props: {
-            sender: "owner",
+            senderName: "owner",
             text: "Ты будешь завтра на вебинаре?",
-            messageId: 3,
+            senderId: 3,
             rootClass: ["message__open", "message__open_right"],
             Time: new TimeInfo({
                 props: {
@@ -139,9 +62,9 @@ const messages = [
     }),
     new Message({
         props: {
-            sender: "companion",
+            senderName: "companion",
             text: "Да, планирую",
-            messageId: 4,
+            senderId: 4,
             rootClass: ["message__open", "message__open_left"],
             Time: new TimeInfo({
                 props: {
@@ -154,9 +77,9 @@ const messages = [
     }),
     new Message({
         props: {
-            sender: "companion",
+            senderName: "companion",
             text: "А ты?",
-            messageId: 5,
+            senderId: 5,
             rootClass: ["message__open", "message__open_left"],
             Time: new TimeInfo({
                 props: {
@@ -169,9 +92,9 @@ const messages = [
     }),
     new Message({
         props: {
-            sender: "owner",
+            senderName: "owner",
             text: "Ага, тоже",
-            messageId: 6,
+            senderId: 6,
             rootClass: ["message__open", "message__open_right"],
             Time: new TimeInfo({
                 props: {
@@ -184,12 +107,38 @@ const messages = [
     })
 ]
 
+async function createChatCallback() {
+    const title = prompt("Введите заголовок нового чата", "")
+    if (title === "") {
+        alert("Нельзя создать чат без имени")
+        return
+    }
+    if (title !== null) {
+        chatsController.create({ title })
+    }
+}
+
+async function deleteChatCallback() {
+    console.log("clicked")
+}
+
+async function addUserCallback() {
+    console.log("clicked")
+}
+
+async function deleteUserCallback() {
+    console.log("clicked")
+}
+
 export class MessengerPage extends Block {
     constructor() {
         const params: IMessengerPageParams = {
             props: {
                 RemovePersonButton: new Button({
                     props: {
+                        events: {
+                            click: [deleteUserCallback]
+                        },
                         rootClass: ["button__navbar"],
                         imgSrc: "person_remove_white_48dp.svg",
                         imgStyle: ["button__image"]
@@ -197,6 +146,9 @@ export class MessengerPage extends Block {
                 }),
                 AddPersonButton: new Button({
                     props: {
+                        events: {
+                            click: [addUserCallback]
+                        },
                         rootClass: ["button__navbar"],
                         imgSrc: "person_add_white_48dp.svg",
                         imgStyle: ["button__image"]
@@ -205,9 +157,12 @@ export class MessengerPage extends Block {
                 rootClass: convertStylesToStrings([styles], "messenger"),
                 SideChatBar: new SideChatBar({
                     props: {
-                        SideChats: sideChatList,
+                        SideChats: [],
                         ChatCreateButton: new Button({
                             props: {
+                                events: {
+                                    click: [createChatCallback]
+                                },
                                 rootClass: ["button__navbar"],
                                 imgSrc: "add_white_48dp.svg",
                                 imgStyle: ["button__image"]
@@ -215,6 +170,9 @@ export class MessengerPage extends Block {
                         }),
                         ChatDeleteButton: new Button({
                             props: {
+                                events: {
+                                    click: [deleteChatCallback]
+                                },
                                 rootClass: ["button__navbar"],
                                 imgSrc: "remove_circle_white_48dp.svg",
                                 imgStyle: ["button__image"]
@@ -225,6 +183,11 @@ export class MessengerPage extends Block {
                                 SearchField: new InputField({ props: {} }),
                                 SearchButton: new Button({
                                     props: {
+                                        events: {
+                                            click: [
+                                                () => console.log("clicked!")
+                                            ]
+                                        },
                                         rootClass: ["button__navbar"],
                                         imgSrc: "search_white_48dp.svg",
                                         imgStyle: ["button__image"]
@@ -257,8 +220,8 @@ export class MessengerPage extends Block {
         super(params)
     }
 
-    componentBeforeMount() {
-        return chatsController.get().then((value) => value)
+    async componentBeforeMount() {
+        await chatsController.get()
     }
 
     render(): HTMLElement {
