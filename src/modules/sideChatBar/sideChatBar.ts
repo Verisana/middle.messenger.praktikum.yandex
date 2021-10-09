8bf0803f4b185fb594a3565ede0caa0d2ebdfd06
Verisana <account@0112.pro>
2021-10-10 02:44:39 +0500

    feat: use global event bus and create select chat method

--- src/modules/sideChatBar/sideChatBar.ts
+++ src/modules/sideChatBar/sideChatBar.ts
@@ -1,9 +1,13 @@
 import styles from "./sideChatBar.css"
+import sideChatStyles from "../sideChat/sideChat.css"
 import sideChatBarTemplate from "./sideChatBar.hbs"
 import { Block } from "../../components/block"
 import { convertStylesToStrings } from "../../utils/utils"
 import { compileToDom } from "../../utils/dom_utils"
 import { ISideChatBarParams } from "./types"
+import { ISideChatProps, SideChat } from "../sideChat"
+import { globalEventBus } from "../../utils/event_bus"
+import { sideChatEvents } from "../sideChat/sideChat"
 
 export class SideChatBar extends Block {
     constructor(params: ISideChatBarParams) {
@@ -14,6 +18,35 @@ export class SideChatBar extends Block {
             props.rootClass
         )
         super(params)
+        globalEventBus().on(
+            sideChatEvents.sideChatClicked,
+            this.sideChatClick.bind(this)
+        )
+    }
+
+    sideChatClick(id: number) {
+        this.selectSideChat(id)
+    }
+
+    selectSideChat(id: number) {
+        const sideChats = this.props.SideChats as SideChat[]
+        const selectStyle = sideChatStyles["side-chat_select"]
+        for (const sideChat of sideChats) {
+            const props = sideChat.props as ISideChatProps
+            if (typeof props.rootClass === "string") {
+                const currentStyles = new Set(props.rootClass.split(" "))
+                if (props.chatId !== id) {
+                    props.selected = false
+                    currentStyles.delete(selectStyle)
+                } else {
+                    props.selected = true
+                    currentStyles.add(selectStyle)
+                }
+                props.rootClass = Array.from(currentStyles).join(" ")
+            } else {
+                throw new Error("rootClass always must be string")
+            }
+        }
     }
 
     render(): HTMLElement {
--- src/modules/sideChatBar/types.ts
+++ src/modules/sideChatBar/types.ts
@@ -1,14 +1,14 @@
 import { BlockParams, Props } from "../../components/block"
 import { Button } from "../../components/button"
-import { SearchBar } from "../searchBar"
+import { SubmitForm } from "../../components/submitForm"
 import { SideChat } from "../sideChat/sideChat"
 
 export interface ISideChatBarProps extends Props {
     SideChats: SideChat[]
     ChatCreateButton: Button
     ChatDeleteButton: Button
+    SearchBar: SubmitForm
     rootClass?: string | string[]
-    SearchBar?: SearchBar
 }
 
 export interface ISideChatBarParams extends BlockParams {
