3d32f1f99dca318770e8dbfae08213ab378558ae
Verisana <account@0112.pro>
2021-10-10 23:21:45 +0500

    feat: update store data types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,11 @@
 import { IChatsResponse, IMessageResponse, UserData } from "./api/types"
+import { SideChat } from "./modules/sideChat"
 
 export interface IStoreData {
     user?: UserData
     chats?: IChatsResponse[]
     messages?: IMessageResponse[]
     chatsSearchQuery?: string
+    usersInChat?: UserData[]
+    selectedChat?: SideChat
 }
