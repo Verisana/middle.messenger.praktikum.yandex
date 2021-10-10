461a47d1d63832b96e53065ed62dcf0ecf99a935
Verisana <account@0112.pro>
2021-10-10 23:25:08 +0500

    feat: preserve selected SideChat when rerendered

--- src/modules/sideChat/sideChat.ts
+++ src/modules/sideChat/sideChat.ts
@@ -3,32 +3,42 @@ import sideChatTemplate from "./sideChat.hbs"
 import layoutStyles from "../../layout/layout.css"
 import { appendEvent, convertStylesToStrings } from "../../utils/utils"
 import { compileToDom } from "../../utils/dom_utils"
-import { defaultAvatar } from "../../consts"
+import { defaultAvatar, globalEvents } from "../../consts"
 import { Block } from "../../components/block"
-import { ISideChatParams } from "./types"
+import { ISideChatParams, ISideChatProps } from "./types"
 import { globalEventBus } from "../../utils/event_bus"
-
-export const sideChatEvents = {
-    sideChatClicked: "sideChatClicked"
-}
+import { store } from "../../store"
 
 function sideChatClick(event: Event) {
     const { currentTarget } = event
     if (currentTarget !== null) {
         globalEventBus().emit(
-            sideChatEvents.sideChatClicked,
+            globalEvents.sideChatClicked,
             Number((currentTarget as HTMLElement).dataset.chatId)
         )
     }
 }
 
+export function isSelectedChat(
+    selected: SideChat | undefined,
+    props: ISideChatProps
+): boolean {
+    return (
+        selected !== undefined &&
+        (selected.props as ISideChatProps).chatId === props.chatId
+    )
+}
+
 export class SideChat extends Block {
     constructor(params: ISideChatParams) {
         const { props } = params
+        const selected = store.select("selectedChat") as SideChat | undefined
+
         props.rootClass = convertStylesToStrings(
             [styles],
             "side-chat",
-            props.rootClass
+            props.rootClass,
+            isSelectedChat(selected, props) ? "side-chat_select" : undefined
         )
         props.chatDivClass = convertStylesToStrings(
             [styles],
@@ -46,8 +56,16 @@ export class SideChat extends Block {
         props.imgStyles = convertStylesToStrings([layoutStyles], "avatar_small")
         props.selected = props.selected === undefined ? false : props.selected
 
+        if (isSelectedChat(selected, props)) {
+            props.selected = true
+        }
+
         props.events = appendEvent("click", sideChatClick, props.events)
         super(params)
+
+        if (isSelectedChat(selected, props)) {
+            store.setValue("selectedChat", this)
+        }
     }
 
     render(): HTMLElement {
