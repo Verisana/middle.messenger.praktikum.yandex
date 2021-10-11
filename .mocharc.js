5dad374e0eaca2f27e802fbf88077013dd9c8cc0
Verisana <account@0112.pro>
2021-10-11 20:34:21 +0500

    build: move mocha into package and new eslint rule

--- .mocharc.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-require("ignore-styles").default([".css", ".hbs"])
-
-module.exports = {
-    require: ["ts-node/register", "ignore-styles", "jsdom-global/register"],
-    reporter: "list",
-    colors: true
-}
